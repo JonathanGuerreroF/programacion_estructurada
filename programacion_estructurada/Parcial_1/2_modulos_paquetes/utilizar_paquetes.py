ad90a31d145fd3a16214c5341c4e9e20a32b2fec
JonathanGuerreroF <jonathan_3041240451@utd.edu.mx>
2025-07-23 08:39:16 -0600

    Delete programacion_estructurada/Parcial_1 directory

--- programacion_estructurada/Parcial_1/2_modulos_paquetes/utilizar_paquetes.py
+++
@@ -1,8 +0,0 @@
-from paquete1 import modulos
-
-print(modulos.saludar("Erik Gabriel Bueno Cao Romero"))
-
-modulos.borrarPantalla()
-nom,tel=modulos.solicitarDatos2()
-print(f"\n\t.::Agenda Telefónica::.\n\t\tNombre:{nom}\n\t\tTelefono:{tel}")
-modulos.espereTecla()
